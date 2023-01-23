5a99d43d4201f7aa51ad6293d01890e8875e4454
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-23 17:56:52 -0300

    ✔️ Feat: classe Race em desenvolvimento

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');    
+  }
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+}
+
+export default Race;

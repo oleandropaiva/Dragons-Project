926c5cd527478dae80e8504c1d8b257230837c34
Leandro Paiva <leandroapaiva@yahoo.com>
2023-01-25 16:42:16 -0300

    ✔️ Feat: req09 classe monter em desenvolvimento

--- src/Monster.1.ts
+++ src/Monster.1.ts
@@ -1,10 +1,11 @@
 import { SimpleFighter } from './Fighter';
 
-class Monster implements SimpleFighter {
+export class Monster implements SimpleFighter {
   protected _lifePoints: number;
   protected _strength: number;
 
   constructor() {
+    super();
     this._lifePoints = 85;
     this._strength = 63;
   }
@@ -17,20 +18,18 @@ class Monster implements SimpleFighter {
     return this._strength;
   }
 
-  receiveDamage(attackPoints: number): number {
-    const damage = attackPoints - this._lifePoints;
-    if (damage > 0) {
-      this._lifePoints -= damage;
-    }
-    if (this.lifePoints <= 0) {
-      this._lifePoints = -1;
-    }
-    return this._lifePoints;
-  }
-
   public attack(enemy: SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
-}
 
-export default Monster;
+  receiDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+      if (this.lifePoints <= 0) {
+        this._lifePoints = -1;
+      }
+    }
+    return this._lifePoints;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+// import Monster from './monster';
